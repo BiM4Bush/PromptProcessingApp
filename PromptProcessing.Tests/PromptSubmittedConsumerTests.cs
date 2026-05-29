3fb5e9be08d840b0d68a601b736a9fe4b8e24924
DESKTOP-RTQ39GI\skrzynowek <sebastian.krzynowek@qed.games>
2026-05-29 18:19:13 +0200

    Added tests;

---
+++ PromptProcessing.Tests/PromptSubmittedConsumerTests.cs
@@ -0,0 +1,58 @@
+// #region HEADER
+// 
+// // Copyright Grail Team 2025
+// 
+// #endregion
+
+using MassTransit;
+using MassTransit.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+using Moq;
+using PromptProcessing.Core.Data;
+using PromptProcessing.Core.Interfaces;
+using PromptProcessing.WorkerService.Consumers;
+using Event = PromptProcessing.Common.Event;
+
+namespace PromptProcessing.Tests;
+
+public class PromptSubmittedConsumerTests
+{
+	[Fact]
+	public async Task Consumer_Should_Receive_Message_And_Trigger_AI_Processing()
+	{
+		// 1. ARRANGE
+		var dbOptions = new DbContextOptionsBuilder<PromptProcessingAppDbContext>()
+			.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+			.Options;
+
+		var kernel = Kernel.CreateBuilder().Build();
+		
+		await using var provider = new ServiceCollection()
+			.AddSingleton(new PromptProcessingAppDbContext(dbOptions))
+			.AddSingleton(kernel)
+			.AddMassTransitTestHarness(x => { x.AddConsumer<PromptSubmittedConsumer>(); })
+			.BuildServiceProvider(true);
+
+		var harness = provider.GetRequiredService<ITestHarness>();
+		await harness.Start();
+
+		try
+		{
+			var testEvent = new Event.PromptSubmittedEvent(Guid.NewGuid());
+
+			// 2. ACT
+			await harness.Bus.Publish(testEvent);
+
+			// 3. ASSERT
+			Assert.True(await harness.Published.Any<Event.PromptSubmittedEvent>());
+			var consumerHarness = harness.GetConsumerHarness<PromptSubmittedConsumer>();
+			Assert.True(await consumerHarness.Consumed.Any<Event.PromptSubmittedEvent>());
+		}
+		finally
+		{
+			await harness.Stop();
+		}
+	}
+}
